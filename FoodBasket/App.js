34a35c7a942862f5e2436c0b482f3855d92cec5b
Eray <eraygenconline@gmail.com>
2023-08-18 11:34:03 +0300

    Project files

---
+++ FoodBasket/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+
+function App() {
+
+
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Hello Group4</Text>
+      </View>
+    </SafeAreaView>
+
+  );
+
+}
+
+
+export default App;
